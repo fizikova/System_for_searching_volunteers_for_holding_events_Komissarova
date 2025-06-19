15aec3747431d90927c01b1778d0f78c892e11cd
fizikova <fizikova228@yandex.ru>
2025-06-20 00:30:30 +0300

    поменяла путь к config.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,18 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 import os
 
+# Инициализация расширений
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
-login.login_view = 'auth.login'  
-
+login.login_view = 'auth.login'
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=False)
 
-   app.config.from_pyfile(
-    os.path.join(os.path.dirname(__file__), '..', 'config.py')
-   ) 
+    app = Flask(__name__)
+
+    config_path = os.path.join(os.path.dirname(__file__), '..', 'config.py')
+    app.config.from_pyfile(config_path)
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -23,8 +23,8 @@ def create_app():
 
     from app.views import main_bp
     app.register_blueprint(main_bp)
+
     from app.views import auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
-
