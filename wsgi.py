dab927eeb4ab2caef03084c10c3240fc153da510
fizikova <fizikova228@yandex.ru>
2025-06-20 05:00:06 +0300

    обнавила логику views и шаблоны аутентификации

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,9 @@
 import sys
 import os
 
-# Указываем путь к корню проекта, чтобы Python мог найти пакет app
 project_home = os.path.abspath(os.path.dirname(__file__))
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# Импортируем фабрику приложения и создаём экземпляр
 from app import create_app
 application = create_app()
